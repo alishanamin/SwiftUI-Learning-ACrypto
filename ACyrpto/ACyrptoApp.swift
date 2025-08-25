aae3ef54ecae9936b2698384fbd13d1cb4bf9b65
Alishan Muhammad Amin <61986083+alishanamin@users.noreply.github.com>
2025-08-25 14:49:47 +0500

    Initial Commit

---
+++ ACyrpto/ACyrptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACyrptoApp.swift
+//  ACyrpto
+//
+//  Created by ALI SHAN Muhammad Amin on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ACyrptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACyrptoTests/ACyrptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ACyrptoTests.swift
+//  ACyrptoTests
+//
+//  Created by ALI SHAN Muhammad Amin on 25/08/2025.
+//
+
+import Testing
+@testable import ACyrpto
+
+struct ACyrptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
